ff0d103999e96de7cd877501c7ab785da8ee339e
Ziad Ziadeh <ziadziadeh@Ziads-MacBook-Pro.local>
2020-05-08 17:14:57 +0300

    Initial Project - Fix path 5

--- js/site.js
+++ js/site.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
       initialCountry: "ps",
       placeholderNumberType: "MOBILE",
       dropdownContainer: document.body,
-      utilsScript: "./intlTellInput/utils.js",
+      utilsScript: "./js/vendors/intlTellInput/utils.js",
     });
     var countryData = window.intlTelInputGlobals.getCountryData(),
       input = document.querySelector("#phone");
@@ -17,7 +17,7 @@ $(document).ready(function () {
   
     window.intlTelInput(input, {
       preferredCountries: ["ps"],
-      utilsScript: "./intlTellInput/utils.js?1585994360633", // just for formatting/placeholders etc
+      utilsScript: "./js/vendors/intlTellInput/utils.js?1585994360633", // just for formatting/placeholders etc
     });
   
     $(".modal-view").click(function () {
